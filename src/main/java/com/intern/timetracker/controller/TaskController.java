20c5eee6ba00b49eb8dbdd59026ce0dd2c8a1d3f
yargames1 <89339132+yargames1@users.noreply.github.com>
2026-05-01 14:56:54 +0300

    feature(controller): create controllers

--- src/main/java/com/intern/timetracker/controller/TaskController.java
+++ src/main/java/com/intern/timetracker/controller/TaskController.java
@@ -1,4 +1,41 @@
 package com.intern.timetracker.controller;
 
+import com.intern.timetracker.dto.Task.CreateTaskRequest;
+import com.intern.timetracker.dto.Task.UpdateTaskStatusRequest;
+import com.intern.timetracker.model.Task;
+import com.intern.timetracker.model.TaskStatus;
+import com.intern.timetracker.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    // Создание задачи
+    @PostMapping
+    public Task createTask(@RequestBody CreateTaskRequest request) {
+        return taskService.createTask(
+                request.getTitle(),
+                request.getDescription()
+        );
+    }
+
+    // Получение информации о задаче по ID
+    @GetMapping("/{id}")
+    public Task getTask(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    // Изменение статуса задачи
+    @PatchMapping("/{id}/status")
+    public void updateStatus(@PathVariable Long id,
+                             @RequestBody UpdateTaskStatusRequest request) {
+        taskService.updateTaskStatus(id, request.getStatus());
+    }
 }
--- src/main/java/com/intern/timetracker/controller/TimeRecordController.java
+++ src/main/java/com/intern/timetracker/controller/TimeRecordController.java
@@ -1,4 +1,47 @@
 package com.intern.timetracker.controller;
 
+import com.intern.timetracker.dto.TimeRecord.CreateTimeRecordRequest;
+import com.intern.timetracker.model.TimeRecord;
+import com.intern.timetracker.service.TimeRecordService;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/time-records")
 public class TimeRecordController {
+
+    private final TimeRecordService timeRecordService;
+
+    public TimeRecordController(TimeRecordService timeRecordService) {
+        this.timeRecordService = timeRecordService;
+    }
+
+    // Создать запись о затраченном времени сотрудника на задачу
+    @PostMapping
+    public TimeRecord createTimeRecord(@RequestBody CreateTimeRecordRequest request) {
+        return timeRecordService.createTimeRecord(
+                request.getEmployeeId(),
+                request.getTaskId(),
+                request.getStartTime(),
+                request.getEndTime(),
+                request.getDescription()
+        );
+    }
+
+    // Получить информацию о затраченном времени сотрудника на задачи за определённый
+    //период времени
+    @GetMapping
+    public List<TimeRecord> getTimeRecords(
+            @RequestParam Long employeeId,
+            @RequestParam String start,
+            @RequestParam String end
+    ) {
+        return timeRecordService.getTimeRecords(
+                employeeId,
+                LocalDateTime.parse(start),
+                LocalDateTime.parse(end)
+        );
+    }
 }
