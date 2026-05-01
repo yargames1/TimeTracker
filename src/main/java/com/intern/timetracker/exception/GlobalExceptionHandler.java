0bb3c1fff24a784a5b6a6bb4064b4b7c9b003f7a
yargames1 <89339132+yargames1@users.noreply.github.com>
2026-05-01 15:27:30 +0300

    feature(exception): create global handler

--- src/main/java/com/intern/timetracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/intern/timetracker/exception/GlobalExceptionHandler.java
@@ -1,7 +1,24 @@
 package com.intern.timetracker.exception;
 
-public class GlobalExceptionHandler extends RuntimeException {
-    public GlobalExceptionHandler(String message) {
-        super(message);
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleOther(Exception ex) {
+        ex.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
     }
 }
